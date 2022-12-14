3368a3ad5c30a7c81b9e7159a1d0b927938832d7
igorjosemartins <igorjosemartins01@gmail.com>
2022-12-14 15:45:07 -0300

    S03EX02 extra

--- modulo1-frontend/semana03/exercicios/ex02/app.js
+++ modulo1-frontend/semana03/exercicios/ex02/app.js
@@ -1,5 +1,25 @@
 
-const mensagemOla = (name) => {
-  return `Olá ${name}`
+// ex: 
+
+// const mensagemOla = (name) => {
+//   return `Olá ${name}`
+// }
+
+// extra:
+
+const button = document.querySelector(".button")
+
+function mensagemOla() {
+  
+  const input = document.querySelector(".txtarea")
+
+  const name = input.value;
+
+  const result = `Olá ${name}`
+
+  const p = document.querySelector(".p")
+
+  p.innerHTML = result
 }
 
+button.addEventListener("click", mensagemOla())
